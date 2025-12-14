c1f7d15fc490a46f9176c4331400029af738c90d
Fabian-C741 <electrofabi.serviciostecnicos@gmail.com>
2025-12-14 19:02:31 -0300

    Fix: session_start antes de config

--- api/buscar_producto.php
+++ api/buscar_producto.php
@@ -1,7 +1,7 @@
 <?php
+session_start();
 require_once '../config/config.php';
 require_once '../includes/Database.php';
-session_start();
 require_once '../includes/functions.php';
 
 header('Content-Type: application/json');
